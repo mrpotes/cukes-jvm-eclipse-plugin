dad22a5cf0332c05e637074881a85a1df569dda6
James Phillpotts <james.github@potes.org.uk>
2012-07-20 22:14:44 +0100

    Fixed #1

--- plugin/src/main/java/potes/cucumberjvm/eclipseplugin/JavaResourceChangeReporter.java
+++ plugin/src/main/java/potes/cucumberjvm/eclipseplugin/JavaResourceChangeReporter.java
@@ -17,15 +17,19 @@
 
 package potes.cucumberjvm.eclipseplugin;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 
@@ -43,7 +47,7 @@ public class JavaResourceChangeReporter implements IResourceChangeListener {
 		try {
 			event.getDelta().accept(new Visitor());
 		} catch (CoreException e) {
-			Activator.getDefault().getLog().log(e.getStatus());
+			Activator.getDefault().getLog().log(new Status(Status.ERROR, e.getStatus().getPlugin(), e.getStatus().getMessage(), e));
 		}
 	}
 	
@@ -54,16 +58,32 @@ public class JavaResourceChangeReporter implements IResourceChangeListener {
 			if ((delta.getKind() == IResourceDelta.REMOVED || delta.getKind() == IResourceDelta.CHANGED) && delta.getResource().getType() == IResource.FILE) {
 				Activator.getDefault().cleanResource(delta.getResource());
 			}
-			IJavaElement javaResource = JavaCore.create(delta.getResource());
+			IJavaElement javaResource = null;
+			try {
+				javaResource = JavaCore.create(delta.getResource());
+			} catch (Exception e) {}
 			if (javaResource != null) {
 //				Activator.getDefault().getLog().log(new Status(Status.INFO, Activator.PLUGIN_ID, javaResource.getClass().getName()+": "+javaResource.toString()));
 				if (javaResource instanceof IType || javaResource instanceof ICompilationUnit) {
 					if (delta.getKind() == IResourceDelta.ADDED || delta.getKind() == IResourceDelta.CHANGED) {
-						Activator.getDefault().checkTypeInProject(javaResource.getJavaProject(), javaResource);
+						Activator.getDefault().checkTypeInProject(getJavaProject(javaResource), javaResource);
 					}
 				}
 			}
 			return true;
+		}
+
+		private IJavaProject getJavaProject(IJavaElement javaResource) throws CoreException {
+			if (javaResource.getJavaProject().getProject().hasNature(JavaCore.NATURE_ID)) {
+				return javaResource.getJavaProject();
+			}
+			for (IJavaProject javaProject : JavaCore.create(ResourcesPlugin.getWorkspace().getRoot()).getJavaProjects()) {
+				for (IContainer container : ResourcesPlugin.getWorkspace().getRoot().findContainersForLocationURI(javaResource.getResource().getLocationURI())) {
+					IProject project = container.getProject();
+					if (javaProject.getProject().equals(project)) return javaProject;
+				}
+			}
+			throw new CoreException(new Status(Status.ERROR, Activator.PLUGIN_ID, "Cannot find java project containing resource: "+javaResource.toString()));
 		}
 		
 	}
