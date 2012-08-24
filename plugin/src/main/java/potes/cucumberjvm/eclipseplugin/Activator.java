d2f40f06245d8d7f63fc6846aa65d21395217d9b
James Phillpotts <james.github@potes.org.uk>
2012-08-24 14:00:42 +0100

    Preliminary i18n work

--- plugin/src/main/java/potes/cucumberjvm/eclipseplugin/Activator.java
+++ plugin/src/main/java/potes/cucumberjvm/eclipseplugin/Activator.java
@@ -31,8 +31,10 @@ import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IAnnotation;
+import org.eclipse.jdt.core.IImportDeclaration;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IMethod;
@@ -55,21 +57,7 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 	
-	private Map<String, IMethod> givenSteps = new HashMap<String, IMethod>();
-	private Map<String, IMethod> thenSteps = new HashMap<String, IMethod>();
-	private Map<String, IMethod> whenSteps = new HashMap<String, IMethod>();
-	private Map<String, IMethod> andSteps = new HashMap<String, IMethod>();
-	private Map<String, IMethod> butSteps = new HashMap<String, IMethod>();
-	
 	private Map<String, Map<String, IMethod>> steps = new HashMap<String, Map<String, IMethod>>();
-	
-	{
-		steps.put("Given", givenSteps);
-		steps.put("Then", thenSteps);
-		steps.put("When", whenSteps);
-		steps.put("And", andSteps);
-		steps.put("But", butSteps);
-	}
 	
 	// The plug-in ID
 	public static final String PLUGIN_ID = "cukes-jvm-plugin"; //$NON-NLS-1$
@@ -121,19 +109,18 @@ public class Activator extends AbstractUIPlugin {
 			throws JavaModelException, CoreException {
 		IType givenType = project.findType("cucumber.annotation.en.Given");
 		if (givenType != null) {
-			IType thenType = project.findType("cucumber.annotation.en.Then");
-			IType whenType = project.findType("cucumber.annotation.en.When");
-			IType andType = project.findType("cucumber.annotation.en.And");
-			IType butType = project.findType("cucumber.annotation.en.But");
-			SearchPattern pattern = SearchPattern.createPattern(givenType, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE);
-			pattern = SearchPattern.createOrPattern(pattern, SearchPattern.createPattern(thenType, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE));
-			pattern = SearchPattern.createOrPattern(pattern, SearchPattern.createPattern(whenType, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE));
-			pattern = SearchPattern.createOrPattern(pattern, SearchPattern.createPattern(andType, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE));
-			pattern = SearchPattern.createOrPattern(pattern, SearchPattern.createPattern(butType, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE));
+			SearchPattern pattern = SearchPattern.createPattern("cucumber.annotation.*.*", IJavaSearchConstants.TYPE, 
+					IJavaSearchConstants.IMPORT_DECLARATION_TYPE_REFERENCE, SearchPattern.R_PATTERN_MATCH);
+//			SearchPattern pattern = SearchPattern.createPattern(givenType, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE);
+//			pattern = SearchPattern.createOrPattern(pattern, SearchPattern.createPattern(thenType, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE));
+//			pattern = SearchPattern.createOrPattern(pattern, SearchPattern.createPattern(whenType, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE));
+//			pattern = SearchPattern.createOrPattern(pattern, SearchPattern.createPattern(andType, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE));
+//			pattern = SearchPattern.createOrPattern(pattern, SearchPattern.createPattern(butType, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE));
 			
 			IJavaSearchScope searchScope = SearchEngine.createJavaSearchScope(element == null ? project.getChildren() : new IJavaElement[]{element}, element == null);
-			searchEngine.search(pattern, new SearchParticipant[] {SearchEngine.getDefaultSearchParticipant()}, 
-					searchScope, requestor, null);
+			SearchParticipant[] participants = new SearchParticipant[] {SearchEngine.getDefaultSearchParticipant()};
+			AnnotationFinder annotationFinder = new AnnotationFinder(project, searchEngine, searchScope, participants);
+			searchEngine.search(pattern, participants, searchScope, annotationFinder, null);
 		}
 	}
 
@@ -154,6 +141,10 @@ public class Activator extends AbstractUIPlugin {
 	public static Activator getDefault() {
 		return plugin;
 	}
+	
+	public static void log(IStatus status) {
+		getDefault().getLog().log(status);
+	}
 
 	/**
 	 * Returns an image descriptor for the image file at the given
@@ -172,40 +163,56 @@ public class Activator extends AbstractUIPlugin {
 		public void acceptSearchMatch(SearchMatch match)
 				throws CoreException {
 			if (!(match.getElement() instanceof IMethod)) {
-				getLog().log(new Status(Status.WARNING, PLUGIN_ID, "Use of cucumber annotation found is not a method: "+match.getElement()));
+				log(new Status(Status.WARNING, PLUGIN_ID, "Use of cucumber annotation found is not a method: "+match.getElement()));
+				return;
 			}
 			IMethod method = (IMethod) match.getElement();
 			for (IAnnotation annotation : method.getAnnotations()) {
 				String annName = annotation.getElementName();
 				String annPackage = method.getDeclaringType().resolveType(annName)[0][0];
 				if (annPackage.startsWith("cucumber.annotation.") && annotation.getMemberValuePairs().length > 0) {
-					String annotationName = annName;
+					Map<String, IMethod> stepMap = getStepMap(annName);
 					String step = annotation.getMemberValuePairs()[0].getValue().toString();
-	//				getLog().log(new Status(Status.INFO, PLUGIN_ID, "Adding step definition ["+step+"] for "+method.getCompilationUnit().getResource()));
-					if ("Given".equals(annotationName)) {
-						givenSteps.put(step, method);
-					} else if ("Then".equals(annotationName)) {
-						thenSteps.put(step, method);
-					} else if ("When".equals(annotationName)) {
-						whenSteps.put(step, method);
-					} else if ("And".equals(annotationName)) {
-						andSteps.put(step, method);
-					} else if ("But".equals(annotationName)) {
-						butSteps.put(step, method);
-					}
+					stepMap.put(step, method);
 				}
 			}
 		}
 		
 	};
 
-	@SuppressWarnings("unchecked")
-	public void cleanResource(IResource resource) {
-		cleanResource(resource, givenSteps, thenSteps, whenSteps, andSteps, butSteps);
-	}
+	private final class AnnotationFinder extends SearchRequestor {
+		
+		private SearchParticipant[] participants;
+		private IJavaSearchScope searchScope;
+		private SearchEngine searchEngine;
+		private IJavaProject project;
 
-	private void cleanResource(IResource resource, Map<String, IMethod>... steps) {
-		for (Map<String, IMethod> stepsMap : steps) {
+		public AnnotationFinder(IJavaProject project, SearchEngine searchEngine, IJavaSearchScope searchScope,
+				SearchParticipant[] participants) {
+			this.project = project;
+			this.searchEngine = searchEngine;
+			this.searchScope = searchScope;
+			this.participants = participants;
+		}
+
+		@Override
+		public void acceptSearchMatch(SearchMatch match)
+				throws CoreException {
+			if (!(match.getElement() instanceof IImportDeclaration)) {
+				Status status = new Status(Status.WARNING, PLUGIN_ID, "Cucumber annotation found is not a type: "+match.getElement());
+				log(status);
+				return;
+			}
+			IImportDeclaration decl = (IImportDeclaration) match.getElement();
+			IType type = project.findType(decl.getElementName());
+			SearchPattern pattern = SearchPattern.createPattern(type, IJavaSearchConstants.ANNOTATION_TYPE_REFERENCE);
+			searchEngine.search(pattern, participants, searchScope, requestor, null);
+		}
+		
+	};
+
+	public void cleanResource(IResource resource) {
+		for (Map<String, IMethod> stepsMap : steps.values()) {
 			Iterator<Entry<String, IMethod>> stepIterator = stepsMap.entrySet().iterator();
 			while (stepIterator.hasNext()) {
 				Entry<String, IMethod> entry = stepIterator.next();
@@ -219,11 +226,9 @@ public class Activator extends AbstractUIPlugin {
 	
 	public Set<IType> getStepDefinitionTypes() {
 		Set<IType> types = new HashSet<IType>();
-		addTypes(types, givenSteps);
-		addTypes(types, whenSteps);
-		addTypes(types, thenSteps);
-		addTypes(types, andSteps);
-		addTypes(types, butSteps);
+		for (Map<String, IMethod> stepMap : steps.values()) {
+			addTypes(types, stepMap);
+		}
 		return types;
 	}
 	
@@ -250,5 +255,14 @@ public class Activator extends AbstractUIPlugin {
 		Map<String, IMethod> stepEntries = steps.get(type);
 		if (stepEntries == null) return null;
 		return new ArrayList<String>(stepEntries.keySet());
+	}
+
+	private synchronized Map<String, IMethod> getStepMap(String annName) {
+		Map<String, IMethod> stepMap = steps.get(annName);
+		if (stepMap == null) {
+			stepMap = new HashMap<String, IMethod>();
+			steps.put(annName, stepMap);
+		}
+		return stepMap;
 	}
 }
